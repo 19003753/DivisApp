68c1c581dda8e68ab28f71dbeae8b7532cbea420
Juan Vargas <sebastian.vargas@galileo.edu>
2025-11-23 11:58:34 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DivisApp19003753',
+  webDir: 'www'
+};
+
+export default config;
