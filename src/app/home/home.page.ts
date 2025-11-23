883ab2d6e3cc671e0cc2efc977c8c49beb40dc0b
Juan Vargas <sebastian.vargas@galileo.edu>
2025-11-23 12:13:26 -0600

    subiendo actividad 3 de desarrollo de apliaciones moviles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
+  quetzales: number | null = null;
+  dolares: number | null = null;
 
   constructor() {}
 
+  convertir() {
+    if (this.quetzales === null || isNaN(Number(this.quetzales))) {
+      this.dolares = null;
+      return;
+    }
+
+    // 1 dolar = 8 quetzales
+    this.dolares = Number(this.quetzales) / 8;
+  }
 }
